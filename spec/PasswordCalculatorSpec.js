a581c5f7e642cae587aab666b7bb22aead0d4973
asradebe <asanda.radebe@umuzi.org>
2019-09-18 17:25:27 +0200

    fixed errors

--- spec/PasswordCalculatorSpec.js
+++ spec/PasswordCalculatorSpec.js
@@ -1,59 +1,27 @@
-describe("password_is_valid()", function() {
-    it("password should be larger than 8 characters",function() {
-        let check = password_is_valid("sugar");
-        expect(check).toBe(false);
+describe("passwordValid", function() {
+    it("password should be greater than 8 characters", function() {
+        expect(() => {password_is_valid("Sthand1")}).toThrow(new Error("password should be greater than 8 characters"))
     });
 
-    it("password should not be null",function() {
-        let check = password_is_valid("sugar");
-        expect(check).toBe(false);
+    it("password should not be null", function() {
+        expect(function() {password_is_valid("")}).toThrow(new Error("password should not be null"))
     });
 
-    it("password should have at least one uppercase letter",function() {
-        let check = password_is_valid("Sugar");
-        expect(check).toBe(false);
+    it("password should have at least one uppercase letter", function() {
+        expect(function() {password_is_valid("sthandiso")}).toThrow(new Error("password should have at least one uppercase letter"))
     });
-    it("password should have at least one lowercase letter",function() {
-        let check = password_is_valid("Sugar");
-        expect(check).toBe(false);
+
+    it("password should have at least one lowercase letter", function() {
+        expect(function() {password_is_valid("STHANDISO")}).toThrow(new Error("password should have at least one lowercase letter"))
     });
-    it("password should have at least one number",function() {
-        let check = password_is_valid("Sugar");
-        expect(check).toBe(false);
+
+    it("password should have at least one digit", function() {
+        expect(function() {password_is_valid("sthandI")}).toThrow(new Error("password should have at least one digit"))
     });
-    it("password should be larger than 8 characters,should not be null and must have at least one lowercase, uppercase letter and one number",function() {
-        let check = password_is_valid("SugarLove23");
-        expect(check).toBe(true);
-    });
+
     
+
+    it("password should meet at least three of the criteria", function() {
+        expect(password_is_okay("sthandiweM3")).toBe(true)
+    });
 });
-
-describe("password_is_okay()", function() {
-    it("password should be greater than 8 characters",function() {
-        let check = password_is_valid("sugar");
-        expect(check).toBe(false);
-    });
-
-    it("password should be greater than 8 characters and have at least one uppercase letter",function() {
-        let check = password_is_valid("sugarlove");
-        expect(check).toBe(false);
-    });
-
-    it("password should be greater than 8 characters and have at least one lowercase letter",function() {
-        let check = password_is_valid("sugarlove");
-        expect(check).toBe(false);
-    }); 
-
-    it("password should be greater than 8 characters and should have at least one number",function() {
-        let check = password_is_valid("sugarlove");
-        expect(check).toBe(false);
-    });
-    it("password should be greater than 8 characters and should not be null",function() {
-        let check = password_is_valid("sugarlove");
-        expect(check).toBe(false);
-    });
-     it("password should be larger than 8 characters,should not be null and must have at least one lowercase, uppercase letter and one number",function() {
-        let check = password_is_valid("SugarLove23");
-        expect(check).toBe(true);
-    });
-})
--- src/PasswordCalculator.js
+++ src/PasswordCalculator.js
@@ -1,37 +1,36 @@
+const uppercase = /[A-Z]/
+const lowercase = /[a-z]/
+const digit = /[0-9]/
+
 function password_is_valid(password){
 
-   let expected = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.{9,}$)/
-   let result = expected.test(password) 
-  
-    try {
-        if (result == false) throw ("password must contain at least one uppercase , lowercase letter and a number");
-            
+        if (password == "") {
+            throw new Error("password should not be null");
         }
-        catch(error) {
-            console.log("ERROR: " + error + ".");
+        if (!uppercase.test(password)) {
+            throw new Error("password should have at least one uppercase letter");
         }
-
-        return result;
-       
+        if (!lowercase.test(password)) {
+            throw new Error("password should have at least one lowercase letter");
+        }
+        if (!digit.test(password)) {
+            throw new Error("password should have at least one digit");
+        }
+        if(password.length < 8) {
+            throw new Error("password should be greater than 8 characters")
+        }
     }
          
 function password_is_okay(password) {
-
-    let testUpper = /[A-Z]/.test(password);
-    let testLower = /[a-z]/.test(password);
-    let digit = /\d/.test(password);
-
-    if ((testUpper) && (testLower) && (digit) && (password.length > 8) && (password !== null)) {
-        return 'Pass';
-     }
- 
-     else {
-         return 'password is not okay';
-     }
-
-   
+     
+    if ((uppercase.test(password)) && (lowercase.test(password)) && (digit.test(password)) && (password.length > 8) && (password !== null)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-password_is_valid();
-password_is_okay();
+//password_is_valid();
+//password_is_okay();
 
 
